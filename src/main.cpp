3dda18cbbb8854113d1ec75a0ec14f7b3fac729b
Gimlihobanka <gimlihobanka@gmail.com>
2020-11-04 21:34:37 +0100

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,168 +1,81 @@
 #include <Arduino.h>
+
 #include <RFM69.h>
+#include <RFM69registers.h>
 #include <SPI.h>
+#include <SPIFlash.h>
 
-// Addresses for this node. CHANGE THESE FOR EACH NODE!
+#define NODEID      99
+#define NETWORKID   100
+#define GATEWAYID   1
+#define FREQUENCY   RF69_868MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
+#define KEY         "thisIsEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
+#define LED         9
+#define SERIAL_BAUD 9600
+//#define ACK_TIME    30  // # of ms to wait for an ack ---- Not Currently Used.
 
-#define NETWORKID     0   // Must be the same for all nodes
-#define MYNODEID      1   // My node ID
-#define TONODEID      2   // Destination node ID
+// Ack will not be used. This is a streaming application. If a packet is lost the next servo position data
+// will be accurate enough to update and move the servo.
 
-// RFM69 frequency, uncomment the frequency of your module:
+//SPIFlash flash(8, 0xEF30);                           //EF40 for 16mbit windbond chip
+RFM69 radio;                                         // initiate radio object
+    
+ int potpin0 = 0;                                    // analog pin used to connect the potentiometer
+ int val0 = 0;                                       // variable initialized for storing potpin0 value
 
-//#define FREQUENCY   RF69_433MHZ
-#define FREQUENCY     RF69_915MHZ
+ typedef struct 
+ {		
+   int           Pot_0;                              //create transmit variable & store data potentiometer 0 data
+ } Payload;
+ Payload theData;                                    // create transmission package 'theData'.
 
-// AES encryption (or not):
-
-#define ENCRYPT       true // Set to "true" to use encryption
-#define ENCRYPTKEY    "TOPSECRETPASSWRD" // Use the same 16-byte key on all nodes
-
-// Use ACKnowledge when sending messages (or not):
-
-#define USEACK        true // Request ACKs or not
-
-// Packet sent/received indicator LED (optional):
-
-#define LED           9 // LED positive pin
-#define GND           8 // LED ground pin
-
-// Create a library object for our RFM69HCW module:
-
-RFM69 radio;
-
-void setup()
+void setup() 
 {
-  // Open a serial port so we can send keystrokes to the module:
-
-  Serial.begin(9600);
-  Serial.print("Node ");
-  Serial.print(MYNODEID,DEC);
-  Serial.println(" ready");  
-
-  // Set up the indicator LED (optional):
-
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,LOW);
-  pinMode(GND,OUTPUT);
-  digitalWrite(GND,LOW);
-
-  // Initialize the RFM69HCW:
-  // radio.setCS(10);  //uncomment this if using Pro Micro
-  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
-  radio.setHighPower(); // Always use this for RFM69HCW
-
-  // Turn on encryption if desired:
-
-  if (ENCRYPT)
-    radio.encrypt(ENCRYPTKEY);
+  Serial.begin(SERIAL_BAUD);
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  radio.setHighPower();                              //uncomment only for RFM69HW!
+  radio.encrypt(KEY);
+  char buff[50];
+  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);                             // Output to serial operating frequency of transceiver       
+  
+  /*
+  if (flash.initialize())
+    Serial.println("SPI Flash Init OK!");          // Output to serial regarding status if Flash memory exists.
+  else
+    Serial.println("SPI Flash Init FAIL! (is chip present?)");
+  */  
 }
 
-
 void Blink(byte PIN, int DELAY_MS)
-// Blink an LED for a given number of ms
 {
+  pinMode(PIN, OUTPUT);
   digitalWrite(PIN,HIGH);
   delay(DELAY_MS);
   digitalWrite(PIN,LOW);
 }
 
-void loop()
+void loop() 
 {
-  // Set up a "buffer" for characters that we'll send:
-
-  static char sendbuffer[62];
-  static int sendlength = 0;
-
-  // SENDING
-
-  // In this section, we'll gather serial characters and
-  // send them to the other node if we (1) get a carriage return,
-  // or (2) the buffer is full (61 characters).
-
-  // If there is any serial input, add it to the buffer:
-
-  if (Serial.available() > 0)
-  {
-    char input = Serial.read();
-
-    if (input != '\r') // not a carriage return
-    {
-      sendbuffer[sendlength] = input;
-      sendlength++;
-    }
-
-    // If the input is a carriage return, or the buffer is full:
-
-    if ((input == '\r') || (sendlength == 61)) // CR or buffer full
-    {
-      // Send the packet!
-
-
-      Serial.print("sending to node ");
-      Serial.print(TONODEID, DEC);
-      Serial.print(", message [");
-      for (byte i = 0; i < sendlength; i++)
-        Serial.print(sendbuffer[i]);
-      Serial.println("]");
-
-      // There are two ways to send packets. If you want
-      // acknowledgements, use sendWithRetry():
-
-      if (USEACK)
-      {
-        if (radio.sendWithRetry(TONODEID, sendbuffer, sendlength))
-          Serial.println("ACK received!");
-        else
-          Serial.println("no ACK received");
-      }
-
-      // If you don't need acknowledgements, just use send():
-
-      else // don't use ACK
-      {
-        radio.send(TONODEID, sendbuffer, sendlength);
-      }
-
-      sendlength = 0; // reset the packet
-      Blink(LED,10);
-    }
-  }
-
-  // RECEIVING
-
-  // In this section, we'll check with the RFM69HCW to see
-  // if it has received any packets:
-
-  if (radio.receiveDone()) // Got one!
-  {
-    // Print out the information:
-
-    Serial.print("received from node ");
-    Serial.print(radio.SENDERID, DEC);
-    Serial.print(", message [");
-
-    // The actual message is contained in the DATA array,
-    // and is DATALEN bytes in size:
-
+ // Output to serial some communications information - Data length, RSSI
     for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]);
+    Serial.print((char)radio.DATA[i]);
+    Serial.print("   [RX_RSSI:");Serial.print(radio.readRSSI());Serial.print("]");
+    Serial.println();
 
-    // RSSI is the "Receive Signal Strength Indicator",
-    // smaller numbers mean higher power.
+ // translate Pot rotation to Servo position.    
+    theData.Pot_0 = map(analogRead(potpin0), 0, 1023, 0, 179);                                              // assign servo position to transmit packet variable.
+  
+ // Transmit payload - theData
+    radio.send(GATEWAYID, (const void*)(&theData), sizeof(theData));   // transmit data to other end
+       
+ // Output to serial information of # bytes sent.
+    Serial.print("Sending struct (");
+    Serial.print(sizeof(theData));
+    Serial.println(" bytes) ");
+    Serial.println();
 
-    Serial.print("], RSSI ");
-    Serial.println(radio.RSSI);
-
-    // Send an ACK if requested.
-    // (You don't need this code if you're not using ACKs.)
-
-    if (radio.ACKRequested())
-    {
-      radio.sendACK();
-      Serial.println("ACK sent");
-    }
-    Blink(LED,10);
-  }
+    Blink(LED,3);                                                      
+    // Board indicator of radio transmitting/receiving.
 }
 
